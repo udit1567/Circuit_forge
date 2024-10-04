bbfee1ab5812673cd4da5c5c75d000e06fea9e09
Udit Maurya <119149306+udit1567@users.noreply.github.com>
2024-10-04 22:32:29 +0530

    Update Home_auto.ino

--- Home Automation with IoT/Home_auto.ino
+++ Home Automation with IoT/Home_auto.ino
@@ -1,1 +1,269 @@
-Home_auto.ino
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+#include <SimpleTimer.h>
+
+
+#define DEBUG_SW 1
+
+
+#define S1 2
+#define R1 16
+#define S2 14
+#define R2 5
+#define S3 13
+#define R3 4
+#define S4 1
+#define R4 12
+
+ 
+
+#define DHTTYPE DHT11     // DHT 11
+
+ 
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); 
+ 
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V6, h);
+}
+
+
+
+int MODE = 0;
+
+
+
+char ssid[] = "JioFi3_36EEE0";
+char pass[] = "#Uditmaurya";
+
+
+char auth[] = "45-ChjWnkE2HISs3DdCUA6Ef4CISCx2q";
+
+
+int switch_ON_Flag1_previous_I = 0;
+int switch_ON_Flag2_previous_I = 0;
+int switch_ON_Flag3_previous_I = 0;
+int switch_ON_Flag4_previous_I = 0;
+
+int wifiFlag = 0;
+
+BLYNK_WRITE(V1)
+{
+  int pinValue = param.asInt(); 
+  digitalWrite(R1, pinValue);
+}
+
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt(); 
+  digitalWrite(R2, pinValue);
+ 
+}
+
+BLYNK_WRITE(V3)
+{
+  int pinValue = param.asInt();
+  digitalWrite(R3, pinValue);
+  
+}
+
+BLYNK_WRITE(V4)
+{
+  int pinValue = param.asInt(); 
+  digitalWrite(R4, pinValue);
+}
+
+void with_internet()
+{
+  if (digitalRead(S1) == LOW)
+  {
+    if (switch_ON_Flag1_previous_I == 1)
+    {
+      digitalWrite(R1, HIGH);
+      if (DEBUG_SW) Serial.println("Relay1- ON");
+      Blynk.virtualWrite(V1, 1);
+      switch_ON_Flag1_previous_I = 0;
+    }
+    if (DEBUG_SW) Serial.println("Switch1 -ON");
+
+  }
+  if (digitalRead(S1) == HIGH )
+  {
+    if (switch_ON_Flag1_previous_I == 0)
+    {
+      digitalWrite(R1, LOW);
+      if (DEBUG_SW) Serial.println("Relay1 OFF");
+      Blynk.virtualWrite(V1, 0);
+      switch_ON_Flag1_previous_I = 1;
+    }
+    if (DEBUG_SW)Serial.println("Switch1 OFF");
+  }
+
+
+  if (digitalRead(S2) == LOW)
+  {
+    if (switch_ON_Flag2_previous_I == 1 )
+    {
+      digitalWrite(R2, HIGH);
+      if (DEBUG_SW)  Serial.println("Relay2- ON");
+      Blynk.virtualWrite(V2, 1);
+      switch_ON_Flag2_previous_I = 0;
+    }
+    if (DEBUG_SW) Serial.println("Switch2 -ON");
+
+  }
+  if (digitalRead(S2) == HIGH )
+  {
+    if (switch_ON_Flag2_previous_I == 0)
+    {
+      digitalWrite(R2, LOW);
+      if (DEBUG_SW) Serial.println("Relay2 OFF");
+      Blynk.virtualWrite(V2, 0);
+      switch_ON_Flag2_previous_I = 1;
+    }
+    if (DEBUG_SW)Serial.println("Switch2 OFF");
+    //delay(200);
+  }
+
+  if (digitalRead(S3) == LOW)
+  {
+    if (switch_ON_Flag3_previous_I == 1 )
+    {
+      digitalWrite(R3,HIGH);
+      if (DEBUG_SW) Serial.println("Relay3- ON");
+      Blynk.virtualWrite(V3, 1);
+      switch_ON_Flag3_previous_I = 0;
+    }
+    if (DEBUG_SW) Serial.println("Switch3 -ON");
+
+  }
+  if (digitalRead(S3) == HIGH )
+  {
+    if (switch_ON_Flag3_previous_I == 0)
+    {
+      digitalWrite(R3, LOW);
+      if (DEBUG_SW) Serial.println("Relay3 OFF");
+      Blynk.virtualWrite(V3, 0);
+      switch_ON_Flag3_previous_I = 1;
+    }
+    if (DEBUG_SW)Serial.println("Switch3 OFF");
+    //delay(200);
+  }
+
+  if (digitalRead(S4) == LOW)
+  {
+    if (switch_ON_Flag4_previous_I == 1 )
+    {
+      digitalWrite(R4, HIGH);
+      if (DEBUG_SW) Serial.println("Relay4- ON");
+      Blynk.virtualWrite(V4, 1);
+      switch_ON_Flag4_previous_I = 0;
+    }
+    if (DEBUG_SW) Serial.println("Switch4 -ON");
+
+  }
+  if (digitalRead(S4) == HIGH )
+  {
+    if (switch_ON_Flag4_previous_I == 0)
+    {
+      digitalWrite(R4, LOW);
+      if (DEBUG_SW) Serial.println("Relay4 OFF");
+      Blynk.virtualWrite(V4, 0);
+      switch_ON_Flag4_previous_I = 1;
+    }
+    if (DEBUG_SW)Serial.println("Switch4 OFF");
+  
+  }
+
+
+
+}
+
+void without_internet()
+{
+
+  digitalWrite(R1, digitalRead(S1));
+  digitalWrite(R2, digitalRead(S2));
+  digitalWrite(R3, digitalRead(S3));
+  digitalWrite(R4, digitalRead(S4));
+
+}
+
+
+void checkBlynk() { 
+
+  bool isconnected = Blynk.connected();
+  if (isconnected == false) {
+    MODE = 1;
+
+  }
+  if (isconnected == true) {
+    MODE = 0;
+
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+ 
+  Blynk.begin(auth, ssid, pass);
+
+ 
+  dht.begin();
+ 
+
+  timer.setInterval(1000L, sendSensor);
+
+  if (DEBUG_SW) Serial.begin(9600);
+  pinMode(S1, INPUT);
+  pinMode(R1, OUTPUT);
+
+  pinMode(S2, INPUT);
+  pinMode(R2, OUTPUT);
+
+  pinMode(S3, INPUT);
+  pinMode(R3, OUTPUT);
+
+  pinMode(S4, INPUT);
+  pinMode(R4, OUTPUT);
+
+ digitalWrite(R1, HIGH) ;
+digitalWrite(R2, HIGH );
+digitalWrite(R3, HIGH) ;
+digitalWrite(R4, HIGH) ;
+
+  WiFi.begin(ssid, pass);
+  timer.setInterval(3000L, checkBlynk);
+  Blynk.config(auth);
+
+}
+
+void loop()
+{  
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi Not Connected");
+  }
+  else
+  {
+    Serial.println("WiFi Connected");
+    Blynk.run();
+  }
+
+  timer.run(); 
+  if (wifiFlag == 0)
+    with_internet();
+  else
+    without_internet();
+}
