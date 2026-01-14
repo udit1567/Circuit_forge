d86953cb3e6a645ccab2612e389d84b97cfb5a7b
Udit Maurya <119149306+udit1567@users.noreply.github.com>
2026-01-14 18:32:33 +0530

    Delete Arduino UNO Q/MPU6050 directory

--- Arduino UNO Q/MPU6050/.cache/sketch/core/new.cpp.d
+++
@@ -1,6 +0,0 @@
-/home/arduino/ArduinoApps/mpu-6050/.cache/sketch/core/new.cpp.o: \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.52.0/cores/arduino/new.cpp \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.52.0/variants/arduino_uno_q_stm32u585xx/llext-edk/include/zephyr/include/generated/zephyr/autoconf.h \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.52.0/variants/arduino_uno_q_stm32u585xx/llext-edk/include/zephyr/include/zephyr/toolchain/zephyr_stdint.h \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.52.0/cores/arduino/new.h \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.52.0/cores/arduino/new
--- Arduino UNO Q/MPU6050/.cache/sketch/libraries/Adafruit GFX Library/glcdfont.c.d
+++
@@ -1,4 +0,0 @@
-/home/arduino/ArduinoApps/mpu-6050/.cache/sketch/libraries/Adafruit\ GFX\ Library/glcdfont.c.o: \
- /home/arduino/.arduino15/internal/Adafruit_GFX_Library_1.12.4_3344e7ed2124dbaf/Adafruit\ GFX\ Library/glcdfont.c \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.52.0/variants/arduino_uno_q_stm32u585xx/llext-edk/include/zephyr/include/generated/zephyr/autoconf.h \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.52.0/variants/arduino_uno_q_stm32u585xx/llext-edk/include/zephyr/include/zephyr/toolchain/zephyr_stdint.h
